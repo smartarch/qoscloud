e143698340cf99adac7d6a8e05d8d2cc824e6e58
Danylo Khalyeyev <khalyeyev@d3s.mff.cuni.cz>
2020-05-15 20:06:14 +0200

    Add support for elasticsearch jobs to ivis job container

--- apps/ivis_job/docker_build.py
+++ apps/ivis_job/docker_build.py
@@ -5,8 +5,8 @@ This script builds the docker images of image client and recognizer server and p
 """
 from subprocess import call
 
-print("Build docker images for frpyc")
-call("docker build -t dankhalev/ivis-job ../..", shell=True)
+print("Building docker image for IVIS jobs")
+call("docker build -t dankhalev/ivis-job -f Dockerfile ../..", shell=True)
 
 print("Pushing images to DockerHub")
 call("docker push dankhalev/ivis-job", shell=True)
