172b9005a208a819b9da5c8de614dc6161f7f9af
Danylo Khalyeyev <khalyeyev@d3s.mff.cuni.cz>
2020-05-23 20:13:47 +0200

    Rewrite IVIS mock to provide REST API instead of gRPC

--- cloud_controller/ivis/ivis_mock.py
+++ cloud_controller/ivis/ivis_mock.py
@@ -1,105 +1,81 @@
 import logging
 import time
 
-import json
-
 from threading import Thread
 
+from cloud_controller import IVIS_CORE_IP, IVIS_CORE_PORT
 from cloud_controller.architecture_pb2 import Architecture, ApplicationType
 from cloud_controller.ivis.ivis_data import SAMPLE_JOB_CODE, SAMPLE_JOB_PARAMETERS, SAMPLE_JOB_INTERVAL, \
     SAMPLE_JOB_CONFIG, SAMPLE_JOB_CREATE_SS_RESPONSE
 from cloud_controller.middleware import AGENT_HOST, AGENT_PORT
-from cloud_controller.middleware.helpers import connect_to_grpc_server, setup_logging, start_grpc_server
-from cloud_controller.middleware.ivis_pb2 import JobAdmissionStatus, JobStatus, JobID, RunParameters, RunFinalStateAck, \
-    RunStatus, RunResponse, JobDescriptor
-from cloud_controller.middleware.ivis_pb2_grpc import IvisCoreServicer, IvisInterfaceStub, \
-    add_IvisCoreServicer_to_server, JobMiddlewareAgentStub
+from cloud_controller.middleware.helpers import connect_to_grpc_server, setup_logging
+from cloud_controller.middleware.ivis_pb2 import JobAdmissionStatus, JobStatus, JobID, RunParameters, JobDescriptor
+from cloud_controller.middleware.ivis_pb2_grpc import IvisInterfaceStub, JobMiddlewareAgentStub
+from cloud_controller.middleware.job_agent import IVIS_HOST, IVIS_PORT
 
-# message RunStatus {
-#     enum Status {
-#         RUNNING = 0;
-#         COMPLETED = 1;
-#         FAILED = 2;
-#     }
-#     string run_id = 1;
-#     Status status = 2;
-#     double start_time = 3;
-#     double end_time = 4;
-#     string stdout = 5;
-#     string stderr = 6;
-#     int32 return_code = 7;
-#     string job_id = 8;
-#     string config = 9; // this field is not used in the current IVIS implementation
-# }
-from cloud_controller.middleware.job_agent import IVIS_CORE_HOST, IVIS_CORE_PORT
+from flask import Flask, request, g, json
 
+app = Flask(__name__)
+state = None
 
 IVIS_INTERFACE_HOST = "0.0.0.0"
 IVIS_INTERFACE_PORT = 7766
-
-
-class IvisCoreMock(IvisCoreServicer):
-
-    def __init__(self):
-        self.state = None
-
-    def HandleRunRequest(self, request, context):
-        request_json = json.loads(request.request)
-        if 'type' in request_json and request_json['type'] == "sets":
-            logging.info(f"Received signal set creation request from job {request.job_id}. Request:\n{request.request}")
-            if self.state is None:
-                self.state = json.loads(SAMPLE_JOB_CREATE_SS_RESPONSE)
-            return RunResponse(response=json.dumps(self.state))
-        elif 'type' in request_json and request_json['type'] == "store":
-            logging.info(f"Received store state request from job {request.job_id}. Request:\n{request.request}")
-            self.state = request_json['state']
-            return RunResponse(response=json.dumps(
-                {"id": request_json['id']}
-            ))
-        else:
-            logging.info(f"Received incorrect runtime request from job {request.job_id}. Request:\n{request.request}")
-            return RunResponse(response=json.dumps(
-                {"error": "Incorrect request format"}
-            ))
-
-    @staticmethod
-    def log_run_status(run_status):
-        logging.info(f"Run {run_status.run_id} of the job {run_status.job_id} was completed successfully in "
-                     f"{run_status.end_time - run_status.start_time} with return code {run_status.return_code}")
-        logging.info("-----------------------------------------------------------------------------\nSTDOUT:")
-        print(run_status.stdout)
-        logging.info("-----------------------------------------------------------------------------\nSTDERR:")
-        print(run_status.stderr)
-
-    def OnSuccess(self, run_status: RunStatus, context):
-        self.log_run_status(run_status)
-        return RunFinalStateAck()
-
-    def OnFail(self, run_status: RunStatus, context):
-        self.log_run_status(run_status)
-        return RunFinalStateAck()
-
-
-# message Architecture {
-#     string name = 1;
-#     map<string, Component> components = 2;
-#     Secret secret = 3;
-#     ApplicationType type = 4;
-#     string job_id = 5;
-#     string code = 6;
-#     string parameters = 7;
-#     string config = 8;
-#     int32 minimal_interval = 9;
-# }
-
 testing_job_agent = False
 
+state_ = None
+
+with app.app_context() as ctx:
+    g.state = None
+    ctx.push()
+
+
+def log_run_status(run_status):
+    logging.info(f"Run {run_status['runId']} of the job {run_status['jobId']} was completed successfully in "
+                 f"{run_status['endTime'] - run_status['startTime']} with return code {run_status['returnCode']}")
+    logging.info("-----------------------------------------------------------------------------\nSTDOUT:")
+    print(run_status['output'])
+    logging.info("-----------------------------------------------------------------------------\nSTDERR:")
+    print(run_status['error'])
+
+
+@app.route("/ccapi/run-request", methods=['POST'])
+def run_request():
+    global state_
+    request_ = request.json
+    print(request_)
+    request_json = json.loads(request_['request'])
+    if 'type' in request_json and request_json['type'] == "sets":
+        logging.info(f"Received signal set creation request from job {request_['jobId']}. Request:\n{request_['jobId']}")
+        if state_ is None:
+            state_ = json.loads(SAMPLE_JOB_CREATE_SS_RESPONSE)
+        return json.jsonify({'response': json.dumps(state_)})
+    elif 'type' in request_json and request_json['type'] == "store":
+        logging.info(f"Received store state request from job {request_['jobId']}. Request:\n{request_['jobId']}")
+        state_ = request_json['state']
+        return json.jsonify({'response': json.dumps({"id": request_json['id']})})
+    else:
+        logging.info(f"Received incorrect runtime request from job {request_['jobId']}. Request:\n{request_['jobId']}")
+        return json.jsonify({'response': json.dumps({"error": "Incorrect request format"})})
+
+
+@app.route("/ccapi/on-success", methods=['POST'])
+def on_success():
+    run_status = request.json
+    log_run_status(run_status)
+    return "200 OK"
+
+
+@app.route("/ccapi/on-fail", methods=['POST'])
+def on_fail():
+    run_status = request.json
+    log_run_status(run_status)
+    return "200 OK"
+
+# TODO: implement /ccapi/on-fail
 
 if __name__ == "__main__":
     setup_logging()
-    ivis_core_mock = IvisCoreMock()
-    ivis_core_thread = Thread(target=start_grpc_server, args=(ivis_core_mock, add_IvisCoreServicer_to_server,
-                                                              IVIS_CORE_HOST, IVIS_CORE_PORT, 10, True))
+    ivis_core_thread = Thread(target=app.run, args=(IVIS_INTERFACE_HOST, IVIS_PORT), daemon=True)
     ivis_core_thread.start()
     if not testing_job_agent:
         ivis_interface: IvisInterfaceStub = connect_to_grpc_server(IvisInterfaceStub,
@@ -140,7 +116,7 @@ if __name__ == "__main__":
     for i in range(4):
         logging.info(f"Executing run {i}")
         ivis_interface.RunJob(RunParameters(job_id="ivisjob", run_id=f"run{i}",
-                                            state=json.dumps(ivis_core_mock.state)))
+                                            state=json.dumps(state)))
         time.sleep(SAMPLE_JOB_INTERVAL)
     if not testing_job_agent:
         # Unschedule the job
