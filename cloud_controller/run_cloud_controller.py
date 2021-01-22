385bf2e8e33dc3007bdedab55e207b4d5cad8838
Danylo Khalyeyev <khalyeyev@d3s.mff.cuni.cz>
2021-01-22 15:54:36 +0100

    Update cloud controller starter

--- cloud_controller/run_cloud_controller.py
+++ cloud_controller/run_cloud_controller.py
@@ -6,16 +6,21 @@ from cloud_controller.extension_manager import ExtensionManager
 from cloud_controller.ivis.ivis_interface import IvisInterface
 from cloud_controller.ivis.ivis_mock import IVIS_INTERFACE_HOST, IVIS_INTERFACE_PORT
 from cloud_controller.ivis.ivis_pb2_grpc import add_IvisInterfaceServicer_to_server
+from cloud_controller.knowledge.knowledge import Knowledge
 from cloud_controller.middleware.helpers import setup_logging, start_grpc_server
 
-if __name__ == "__main__":
-    # TONOWDO: move ivis interface start elsewhere
-    setup_logging()
-    ClusterCleaner(PRODUCTION_MONGOS_SERVER_IP).cleanup()
-    adaptation_ctl = ExtensionManager().get_adaptation_ctl()
-    ivis_interface = IvisInterface(adaptation_ctl.knowledge)
+
+def start_ivis(knowledge: Knowledge):
+    ivis_interface = IvisInterface(knowledge)
     ivis_interface_thread = threading.Thread(
         target=start_grpc_server,
         args=(ivis_interface, add_IvisInterfaceServicer_to_server, IVIS_INTERFACE_HOST, IVIS_INTERFACE_PORT, 10, True))
     ivis_interface_thread.start()
+
+
+if __name__ == "__main__":
+    setup_logging()
+    ClusterCleaner(PRODUCTION_MONGOS_SERVER_IP).cleanup()
+    adaptation_ctl = ExtensionManager().get_adaptation_ctl()
+    start_ivis(adaptation_ctl.knowledge)
     adaptation_ctl.run()
--- run_production.sh
+++ run_production.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PYTHONPATH=.:predictor/src
-python3 cloud_controller/main.py "$@"
+python3 cloud_controller/run_cloud_controller.py "$@"
